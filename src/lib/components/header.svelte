fee43573cc84645f44ec4d5c4fd5284d46bd9f87
Kevin Lingk <klingk.msg@gmail.com>
2025-09-06 23:20:36 +0200

    feat: replace header with AppBar component

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,1 +1,16 @@
-<p>Header</p>
+<script>
+    import { AppBar } from '@skeletonlabs/skeleton-svelte';
+</script>
+
+<AppBar classes="px-10" trailClasses="space-x-4 items-center">
+   {#snippet lead()}
+        <h2 class="h2">Portfolio</h2>
+    {/snippet}
+
+    {#snippet trail()}
+        <a href="/" class="hover:text-success-500 transition-colors">Home</a>
+        <a href="/projects" class="hover:text-success-500 transition-colors">Projects</a>
+        <a href="/github" class="hover:text-success-500 transition-colors">GitHub</a>
+        <a href="/contact" class="hover:text-success-500 transition-colors">Contact</a>
+    {/snippet}
+</AppBar>

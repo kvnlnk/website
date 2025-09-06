735d24df1016c62ea780dc633756d8b142fc8a77
Kevin Lingk <klingk.msg@gmail.com>
2025-09-06 23:37:25 +0200

    style: run prettier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,15 @@
 <script>
 	import '../app.css';
-	import Header from "$lib/components/header.svelte"
-	import Footer from "$lib/components/footer.svelte"
+	import Header from '$lib/components/header.svelte';
+	import Footer from '$lib/components/footer.svelte';
 
 	let title = 'SvelteKit App';
 </script>
 
-
-
 <div class="grid h-screen grid-rows-[auto_1fr_auto]">
 	<!-- Header -->
-	<header class="sticky top-0 z-10 bg-red-500/80 backdrop-blur-sm p-4">
-		<Header/>
+	<header class="sticky top-0 z-10 bg-red-500/80 p-4 backdrop-blur-sm">
+		<Header />
 	</header>
 
 	<!-- Main -->
@@ -21,6 +19,6 @@
 
 	<!-- Footer -->
 	<footer class="bg-blue-500 p-4">
-		<Footer/>
+		<Footer />
 	</footer>
 </div>

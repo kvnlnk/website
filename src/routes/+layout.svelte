8455ec1fd60708d692ae4a694d4a8efc32639ed3
Kevin Lingk <klingk.msg@gmail.com>
2025-09-06 17:09:05 +0200

    feat: add page layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,28 @@
 <script>
 	import '../app.css';
-	export let title = 'SvelteKit App';
+	import { Header } from "$lib/components/header.svelte"
+	import { Footer } from "$lib/components/footer.svelte"
+
+	let title = 'SvelteKit App';
 </script>
 
 <svelte:head>
 	<title>{title}</title>
 </svelte:head>
 
-<div class="h-screen w-screen flex flex-col">
-	<header class="p-4 border-b border-base-300 text-center">
-		<h1 class="text-3xl font-bold">My SvelteKit App</h1>
+<div class="grid h-screen grid-rows-[auto_1fr_auto]">
+	<!-- Header -->
+	<header class="sticky top-0 z-10 bg-red-500/80 backdrop-blur-sm p-4">
+		<Header/>
 	</header>
-	<main class="flex-grow p-4 text-center flex flex-col justify-center items-center">
+
+	<!-- Main -->
+	<main class="">
 		<slot />
 	</main>
-	<footer class="p-4 border-t border-base-300 text-center">
-		<p>&copy; 2024 My SvelteKit App</p>
+
+	<!-- Footer -->
+	<footer class="bg-blue-500 p-4">
+		<Footer/>
 	</footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="h-full w-full bg-green-500 p-4 space-y-4 overflow-y-auto">
+    <p class="h-full bg-purple-500">1</p>
+    <p class="h-2/3 bg-purple-500 p-4">2</p>
+    <p class="h-2/3 bg-purple-500 p-4">3</p>
+</div>

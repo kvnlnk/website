d2ca5224b95f75c14ecb213a873e89ce76a50e6d
Kevin Lingk <klingk.msg@gmail.com>
2025-09-06 23:37:02 +0200

    style: add text underline animation

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,16 +1,32 @@
 <script>
-    import { AppBar } from '@skeletonlabs/skeleton-svelte';
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
 </script>
 
 <AppBar classes="px-10" trailClasses="space-x-4 items-center">
-   {#snippet lead()}
-        <h2 class="h2">Portfolio</h2>
-    {/snippet}
+	{#snippet lead()}
+		<h2 class="h2">Portfolio</h2>
+	{/snippet}
 
-    {#snippet trail()}
-        <a href="/" class="hover:text-success-500 transition-colors">Home</a>
-        <a href="/projects" class="hover:text-success-500 transition-colors">Projects</a>
-        <a href="/github" class="hover:text-success-500 transition-colors">GitHub</a>
-        <a href="/contact" class="hover:text-success-500 transition-colors">Contact</a>
-    {/snippet}
+	{#snippet trail()}
+		<a href="/" class="transition-colors hover:text-success-500">Home</a>
+		<a href="/projects" class="transition-colors hover:text-success-500">Projects</a>
+		<a href="/github" class="transition-colors hover:text-success-500">GitHub</a>
+		<a href="/contact" class="transition-colors hover:text-success-500">Contact</a>
+	{/snippet}
 </AppBar>
+
+<style>
+	/* Credit: https://stackoverflow.com/questions/62867788/text-underline-animation-in-css-the-simplest-way */
+	a {
+		background: linear-gradient(currentColor 0 0) bottom left/ var(--underline-width, 0%) 0.1em
+			no-repeat;
+		display: inline-block;
+		padding: 0 0.5em 0.2em;
+		text-decoration: none;
+		transition: background-size 0.5s;
+	}
+
+	a:hover {
+		--underline-width: 100%;
+	}
+</style>

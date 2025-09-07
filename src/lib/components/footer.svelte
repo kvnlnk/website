ea11d7a762e61b88c71a08c8eee31e05dbd6c653
Kevin Lingk <klingk.msg@gmail.com>
2025-09-07 12:09:07 +0200

    feat: add LinkedIn link and open hrefs in new tabs

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -2,14 +2,14 @@
     let year = new Date().getFullYear()
     let email = "kevin.lingk@proton.me"
     let github = "https://github.com/kvnlnk"
-    let linkedIn = ""
+    let linkedIn = "https://www.linkedin.com/in/kevin-lingk-471595360"
 </script>
 
 <div class="flex flex-col items-center gap-y-6 py-4">
     <div class="flex flex-row gap-4">
-        <a href="mailto:{ email }">E-Mail</a>
-        <a href={github}>GitHub</a>
-        <a href={linkedIn}>LinkedIn</a>
+        <a target="_blank" href="mailto:{ email }">E-Mail</a>
+        <a target="_blank" href={github}>GitHub</a>
+        <a target="_blank" href={linkedIn}>LinkedIn</a>
     </div>
     <div>
         <p class="text-center text-sm">

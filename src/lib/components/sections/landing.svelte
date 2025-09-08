2f02b74c5470367bdeb59d66abe27b22b1748ac3
kvnlnk <klingk.msg@gmail.com>
2025-09-08 13:38:14 +0200

    style: enhance navigation buttons with gradient styles and hover effects

--- src/lib/components/sections/landing.svelte
+++ src/lib/components/sections/landing.svelte
@@ -14,8 +14,64 @@
 
     <!-- Add buttons for navigation -->
     <div class="mt-8 flex flex-row gap-4 p-4">
-        <a href="#projects" class="bg-success-500 text-black px-4 py-2 rounded hover:bg-success-600 transition-colors">View Projects</a>
-        <a href="#github" class="bg-surface-700 text-white px-4 py-2 rounded hover:bg-surface-600 transition-colors">View GitHub</a>
+        <a href="#projects" class="px-4 py-2 rounded  transition-colors b1">
+                <span class="text1">View Projects</span>
+        </a>
+        <a href="#github" class="bg-surface-700 text-white px-4 py-2 rounded hover:bg-surface-600 transition-colors b2">
+           <span class="text2">View GitHub</span>
+        </a>
     </div>
   </div>
 </div>
+
+<style>
+   /* Credit: https://codepen.io/kay8/pen/azKbjN*/
+.b1{
+  background: linear-gradient(to right, var(--color-success-600) 50%, white 50%);
+  background-size: 200% 100%;
+  background-position: right bottom;
+  transition: all .5s ease-out;
+}
+
+.b2{
+  background: linear-gradient(to right, white 50%, var(--color-success-600) 50%);
+  background-size: 200% 100%;
+  background-position: right bottom;
+  transition: all .5s ease-out;
+}
+
+.b1:hover, .b2:hover {
+  background-position: left bottom;
+}
+
+.text1, .text2 {
+  text-align: center;
+  font-size: 16px;
+  line-height: 30px; 
+  transition: all .6s ease-out;
+}
+
+.text1 {
+  color: var(--color-success-500);
+}
+
+.text2 { 
+  color: white;
+}
+
+.b1:hover .text1 {
+    color: white;  
+}
+
+.b1:hover {
+    box-shadow: 0 2px 16px white;
+}
+
+.b2:hover {
+    box-shadow: 0 2px 16px var(--color-success-500);
+}
+
+.b2:hover .text2 {
+  color: var(--color-success-500);
+}
+</style>

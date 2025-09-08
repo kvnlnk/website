ab2f20b81758da147fd13d7c2e95a755f0ed7041
Kevin Lingk <klingk.msg@gmail.com>
2025-09-08 21:03:01 +0200

    feat: add section headline with short underline

--- src/lib/components/sections/projects.svelte
+++ src/lib/components/sections/projects.svelte
@@ -1,1 +1,21 @@
-<p id="projects">Projects Section</p>
+<div class="flex h-full w-full flex-col items-center justify-center gap-4" id="projects">
+	<h2 class="short-underline p-4 text-5xl font-bold whitespace-nowrap text-white">My Projects</h2>
+</div>
+
+<style>
+    /* Credit: https://stackoverflow.com/questions/23502675/css-underline-less-than-width-of-headline */
+	.short-underline {
+		display: inline-block;
+		position: relative;
+	}
+
+	.short-underline::after {
+		content: '';
+		position: absolute;
+		left: 25%;
+		bottom: -0.125rem;
+		width: 50%;
+		height: 0.3rem;
+		background: var(--color-success-500);
+	}
+</style>

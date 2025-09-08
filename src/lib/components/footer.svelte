9755b193062d9bcd5bd5cb15f7f96d8dcd9a50ce
kvnlnk <klingk.msg@gmail.com>
2025-09-08 09:29:00 +0200

    chore: update dependencies and improve footer styles

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -7,14 +7,14 @@
 
 <div class="flex flex-col items-center gap-y-6 py-4">
 	<div class="flex flex-row gap-4">
-		<a target="_blank" href="mailto:{email}" class="transition-colors hover:text-success-500"
+		<a target="_blank" href="mailto:{email}" class="transition-colors hover:text-success-500 text-white"
 			>E-Mail</a
 		>
-		<a target="_blank" href={github} class="transition-colors hover:text-success-500">GitHub</a>
-		<a target="_blank" href={linkedIn} class="transition-colors hover:text-success-500">LinkedIn</a>
+		<a target="_blank" href={github} class="transition-colors hover:text-success-500 text-white">GitHub</a>
+		<a target="_blank" href={linkedIn} class="transition-colors hover:text-success-500 text-white">LinkedIn</a>
 	</div>
 	<div>
-		<p class="text-center text-sm">
+		<p class="text-center text-sm text-white">
 			&copy; {year} kvnlnk. Created with lots of coffee.
 		</p>
 	</div>

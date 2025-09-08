26b5a3f9d114de123dec4301bb7ca843190d9a53
kvnlnk <klingk.msg@gmail.com>
2025-09-08 15:07:54 +0200

    style: format code for consistency and readability

--- src/lib/components/sections/github.svelte
+++ src/lib/components/sections/github.svelte
@@ -1,12 +1,14 @@
 <script>
-    import { GITHUB } from '$lib/config.js';
+	import { GITHUB } from '$lib/config.js';
 </script>
 
 <div class="flex h-full w-full flex-col items-center justify-center gap-4" id="github">
-
-        <h2 class="bg-gradient-to-r from-white via-success-500 to-success-500 bg-clip-text p-4 text-5xl font-bold whitespace-nowrap text-transparent">Discover my code</h2>
-        <a href={GITHUB} class="text-3xl text-surface-400 hover:text-surface-200">
-            Visit GitHub Profile
-        </a>
-
+	<h2
+		class="bg-gradient-to-r from-white via-success-500 to-success-500 bg-clip-text p-4 text-5xl font-bold whitespace-nowrap text-transparent"
+	>
+		Discover my code
+	</h2>
+	<a target="_blank" href={GITHUB} class="text-3xl text-surface-400 hover:text-surface-200">
+		Visit GitHub Profile
+	</a>
 </div>

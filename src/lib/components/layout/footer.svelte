e8ca7a1fde85e911e9d63a275716f8e3658f2d07
kvnlnk <klingk.msg@gmail.com>
2025-09-08 11:45:04 +0200

    refactor: refactor footer links to use constants from config.js

--- src/lib/components/layout/footer.svelte
+++ src/lib/components/layout/footer.svelte
@@ -1,17 +1,16 @@
 <script>
+	import { EMAIL, GITHUB, LINKEDIN } from '$lib/config.js';
 	let year = new Date().getFullYear();
-	let email = 'kevin.lingk@proton.me';
-	let github = 'https://github.com/kvnlnk';
-	let linkedIn = 'https://www.linkedin.com/in/kevin-lingk-471595360';
+
 </script>
 
 <div class="flex flex-col items-center gap-y-6 py-4">
 	<div class="flex flex-row gap-4">
-		<a target="_blank" href="mailto:{email}" class="transition-colors hover:text-success-500 text-white"
+		<a target="_blank" href="mailto:{EMAIL}" class="transition-colors hover:text-success-500 text-white"
 			>E-Mail</a
 		>
-		<a target="_blank" href={github} class="transition-colors hover:text-success-500 text-white">GitHub</a>
-		<a target="_blank" href={linkedIn} class="transition-colors hover:text-success-500 text-white">LinkedIn</a>
+		<a target="_blank" href={GITHUB} class="transition-colors hover:text-success-500 text-white">GitHub</a>
+		<a target="_blank" href={LINKEDIN} class="transition-colors hover:text-success-500 text-white">LinkedIn</a>
 	</div>
 	<div>
 		<p class="text-center text-sm text-white">
---
+++ src/lib/config.js
@@ -0,0 +1,3 @@
+export const EMAIL = 'kevin.lingk@proton.me';
+export const GITHUB = 'https://github.com/kvnlnk';
+export const LINKEDIN = 'https://www.linkedin.com/in/kevin-lingk-471595360';

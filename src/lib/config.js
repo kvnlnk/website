46fcee30c208a24b9cbfb650188127af132dd6b3
kvnlnk <klingk.msg@gmail.com>
2025-09-09 11:40:40 +0200

    feat: add projects array with portfolio and smart water bottle details (temporary)

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,20 @@
 export const EMAIL = 'kevin.lingk@proton.me';
 export const GITHUB = 'https://github.com/kvnlnk';
 export const LINKEDIN = 'https://www.linkedin.com/in/kevin-lingk-471595360';
+
+export const PROJECTS = [
+    {
+        title: 'Portfolio Website',
+        description: 'My personal portfolio website built with SvelteKit and Tailwind CSS.',
+        imgSrc: 'images/projects/portfolio_website.png',
+        link: 'https://github.com/kvnlnk/website',
+        tags: ['SvelteKit', 'Tailwind CSS', 'JavaScript']
+    },
+    {
+        title: 'Smart Water Bottle',
+        description: 'An intelligent water bottle system for monitoring and promoting healthy drinking habits build with Flutter, C# and C++.',
+        imgSrc: 'images/projects/smart_water_bottle.png',
+        link: 'https://github.com/kvnlnk/swpm-smart-water-bottle',
+        tags: ['Flutter', 'C#','FastEndpoints', 'C++', 'Supabase', 'ESP32']
+    }
+];

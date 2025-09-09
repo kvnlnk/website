6e89f8233fa2cb7eab1ded7d3424e801e4d4ca14
Kevin Lingk <klingk.msg@gmail.com>
2025-09-09 20:37:14 +0200

    style: update projectTile component for improved responsiveness and layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,6 @@
 
 <div class="h-full w-full overflow-y-auto bg-green-500">
 	<div class="h-screen bg-surface-950 p-4"><Landing /></div>
-	<!--
 	<div class="h-fit bg-surface-900 p-4"><Projects /></div>
-	-->
 	<div class="h-fit bg-surface-950 p-4"><GitHub /></div>
 </div>
